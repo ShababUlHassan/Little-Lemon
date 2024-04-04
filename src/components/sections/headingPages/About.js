c4b0defbf0e4c46df80b8d33ea2689c8426f0e29
ShababUlHassan <148527718+ShababUlHassan@users.noreply.github.com>
2024-04-04 07:30:11 +0500

    Update About.js

--- src/components/sections/headingPages/About.js
+++ src/components/sections/headingPages/About.js
@@ -4,9 +4,9 @@ export default function About() {
             <section className="hero-text">
                 <h1>Little Lemon</h1>
                 <h2>Chicago</h2>
-                <p className="about-subtext">Little Lemon opened in 1995 by two Italian brothers, Adrian and Mario. 
-                Despite the city's diversity, the two brothers recognized the lack of Mediterranean cuisine in Chicago, 
-                and were inspired to bring the flavors of their hometown in Italy to the people of Chicago. The two brothers 
+                <p className="about-subtext">Little Lemon opened in 2015 by two brothers, john and kate. 
+                Despite the city's diversity, the two brothers recognized the lack of Mediterranean cuisine in london, 
+                and were inspired to bring the flavors of their hometown in Italy to the people of US. The two brothers 
                 continue to oversee the Little Lemon restaurant, nearly thirty years later.</p>
             </section>
 
